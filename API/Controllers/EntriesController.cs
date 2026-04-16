3ac85eb27e18240768a9776b4cb69946c08a67c8
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:52:07 -0600

    feat: enpoint para editar las entradas

--- API/Controllers/EntriesController.cs
+++ API/Controllers/EntriesController.cs
@@ -25,6 +25,33 @@ namespace API.Controllers
             return Ok(history);
         }
 
+        [HttpPut]
+        [Route("UpdateEntry/{id}")]
+        public async Task<IActionResult> UpdateEntry(int id, [FromBody] EntryUpdateDto dto)
+        {
+            if(id != dto.Id)
+            {
+                return BadRequest(new
+                {
+                    message = "El ID de la URL no coincide con la petición"
+                });
+            }
+
+            var success = await _entryService.UpdateEntryAsync(dto);
+            if (!success)
+            {
+                return NotFound(new
+                {
+                    message = "No se encontró el registro de entrada para actualizar"
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Entrada actualizada correctamente"
+            });
+        }
+
         [HttpPost]
         [Route("Create")]
         public async Task<IActionResult> Create(EntryCreateDto dto)
