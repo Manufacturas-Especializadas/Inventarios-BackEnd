d500cfccf891212173999a271ffed071773df994
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 08:50:06 -0600

    feat: dto para editar salidas

---
+++ Application/DTOs/ExitUpdateDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitUpdateDto
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+
+        public string ShopOrder1 { get; set; } = string.Empty;
+
+        public string? ShopOrder2 { get; set; }
+
+        public string? ShopOrder3 { get; set; }
+
+        public string? ShopOrder4 { get; set; }
+
+        public string? ShopOrder5 { get; set; }
+
+        public string? ShopOrder6 { get; set; }
+
+        public List<ExitDetailDto> Details { get; set; } = new();
+    }
+}
