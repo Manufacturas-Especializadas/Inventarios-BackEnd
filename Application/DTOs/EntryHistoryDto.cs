06052f8c29a22e3d16ddaab5efdafd9c34e39d6b
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:47:18 -0600

    feat: dto para obtener las entradas de forma individual

---
+++ Application/DTOs/EntryHistoryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class EntryHistoryDto
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<EntryDetailDto> Details { get; set; } = new();
+
+    }
+}
