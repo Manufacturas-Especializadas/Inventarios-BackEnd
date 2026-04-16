d43c2c4029852ee152b8529a3cc69e94a41bc5bc
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:19:40 -0600

    feat: uso de la interfaz de actualizar entradas

--- Infrastructure/Repositories/EntryRepository.cs
+++ Infrastructure/Repositories/EntryRepository.cs
@@ -26,6 +26,22 @@ namespace Infrastructure.Repositories
                     .FirstOrDefaultAsync(e => e.Id == id);
         }
 
+        public async Task<bool> UpdateEntryAsync(EntryHeader entry)
+        {
+            var existingEntry = await _context.EntryHeaders
+                        .Include(e => e.Details)
+                        .FirstOrDefaultAsync(e => e.Id == entry.Id);
+
+            if (existingEntry == null) return false;
+
+            _context.EntryDetails.RemoveRange(existingEntry.Details);
+            existingEntry.Details = entry.Details;
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
         public async Task<int> CreateEntryAsync(EntryHeader entry)
         {
             _context.EntryHeaders.Add(entry);
