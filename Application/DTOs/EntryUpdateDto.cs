5a7c6909dcfdeff93e70988a822b03a9a0c720e5
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:47:29 -0600

    feat: dto para editar una entrada

---
+++ Application/DTOs/EntryUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class EntryUpdateDto
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+
+        public List<EntryDetailDto> Details { get; set; } = new();
+    }
+}
