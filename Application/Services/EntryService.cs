cddaf302aeeaa6bf718ce359e1d2d4e59e546d32
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:32:19 -0600

    feat: metodo para obtener las entradas de forma individual

--- Application/Services/EntryService.cs
+++ Application/Services/EntryService.cs
@@ -13,6 +13,24 @@ namespace Application.Services
             _repository = repository;
         }
 
+        public async Task<List<EntryHistoryDto>> GetEntriesHistoryAsync(int lineId)
+        {
+            var entries = await _repository.GetEntriesHistoryByLineAsync(lineId);
+
+            return entries.Select(e => new EntryHistoryDto
+            {
+                Id = e.Id,
+                LineId = e.LineId,
+                CreatedAt = e.CreatedAt,
+                Details = e.Details.Select(d => new EntryDetailDto
+                {
+                    PartNumber = d.PartNumber,
+                    Client = d.Client,
+                    Quantity = d.Quantity
+                }).ToList()
+            }).ToList();
+        }
+
         public async Task<int> RegisterEntryAsync(EntryCreateDto dto)
         {
             TimeZoneInfo mexicoTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time (Mexico)");
