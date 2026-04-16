4a6b712f55b8697f19f3b63ef04cae74fd7e4f2c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-16 09:07:15 -0600

    feat: dto para salidas individuales

---
+++ Application/DTOs/ExitHistoryDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitHistoryDto
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+
+        public string ShopOrder1 { get; set; } = string.Empty;
+
+        public string? ShopOrder2 { get; set; }
+
+        public string? ShopOrder3 { get; set; }
+
+        public string? ShopOrder4 { get; set; }
+
+        public string? ShopOrder5 { get; set; }
+
+        public string? ShopOrder6 { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<ExitDetailDto> Details { get; set; } = new();
+
+    }
+}
