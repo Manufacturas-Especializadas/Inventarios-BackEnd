f0c453b9873abe2ccc7569021019a8be3cd92b85
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-29 07:37:28 -0600

    feat: Dto para salida por folio de linea 12

---
+++ Application/DTOs/ExitByFolioDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitByFolioDto
+    {
+        public int LineId { get; set; }
+
+        public string Folio { get; set; }
+    }
+}
