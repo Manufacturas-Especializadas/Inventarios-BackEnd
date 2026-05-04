a6ce4e5262bd23484e649f29ae4d58953a3d139f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-04 07:50:59 -0600

    feat(DTO): dto para generar el reporte por folios

---
+++ Application/DTOs/ExitReportDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitReportDto
+    {
+        public string Folio { get; set; }
+
+        public string ShopOrder { get; set; }
+
+        public string PartNumber { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
