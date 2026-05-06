8afcfac73cd8ac65dd6e267e35a0ec3655c5dba0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-06 09:06:43 -0600

    feat(enpoint): Este endpoint nos devuelve los logs de los reportes

--- API/Controllers/ExitsController.cs
+++ API/Controllers/ExitsController.cs
@@ -72,6 +72,26 @@ namespace API.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("report-logs/{lineId}")]
+        public async Task<IActionResult> GetReportLogs(int lineId)
+        {
+            var logs = await _exitService.GetReportLogsAsync(lineId);
+
+            var result = logs.Select(log => new
+            {
+                id = log.Id,
+                printedAt = log.PrintedAt,
+                details = log.Details.Select(d => new
+                {
+                    folio = d.Folio,
+                    isProcessed = d.IsProcessed
+                }).ToList()
+            });
+
+            return Ok(result);
+        }
+
         [HttpPost]
         [Route("CreateExit")]
         public async Task<IActionResult> Create(ExitCreateDto dto)
