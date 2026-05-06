eb8f85c85bb58a2f6172b32a039f71c6b5a6e26a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-06 07:51:15 -0600

    feat(entities): Nuevas entidades para generar historial de reportes

---
+++ Application/DTOs/ExitReportLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitReportLog
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+ 
+        public List<ExitReportLogDetail> Details { get; set; } = new List<ExitReportLogDetail>();
+    }
+}
---
+++ Application/DTOs/ExitReportLogDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class ExitReportLogDetail
+    {
+        public int Id { get; set; }
+
+        public int ExitReportLogId { get; set; }
+
+        public string Folio { get; set; } = string.Empty;
+
+        public bool IsProcessed { get; set; } = false;
+
+        public ExitReportLog ReportLog { get; set; } = null!;
+    }
+}
