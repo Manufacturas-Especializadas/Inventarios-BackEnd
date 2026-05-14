2325ad7affa673ff96e73b62ce179e3fa6513534
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-14 14:55:59 -0600

    feat(api): endpoint que procesa los folios de FTN

--- API/Controllers/FtnController.cs
+++ API/Controllers/FtnController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services;
+﻿using Application.Interfaces;
+using Application.Services;
 using Domain.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,12 +12,14 @@ namespace API.Controllers
     {
         private readonly FtnReconciliationService _reconciliationService;
         private readonly IFtnInventoryRepository _ftnRepository;
+        private readonly IExcelReaderService _excelReaderService;
 
         public FtnController(FtnReconciliationService reconciliationService,
-            IFtnInventoryRepository ftnRepository)
+            IFtnInventoryRepository ftnRepository, IExcelReaderService excelReaderService)
         {
             _reconciliationService = reconciliationService;
             _ftnRepository = ftnRepository;
+            _excelReaderService = excelReaderService;
         }
 
         [HttpGet]
@@ -32,9 +35,21 @@ namespace API.Controllers
         [Route("reconcile/{lineId}")]
         public async Task<IActionResult> Reconcile(int lineId, IFormFile file)
         {
-            if (file == null || file.Length == 0) return BadRequest("Archivo no proprocionado");
+            if (file == null || file.Length == 0) return BadRequest("Archivo no proporcionado o vacio");
 
-            var foliosFromExcel = new List<string>();
+            List<string> foliosFromExcel;
+
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+
+                foliosFromExcel = _excelReaderService.ExtractFoliosFromStream(stream);
+            }
+
+            if(foliosFromExcel.Count == 0)
+            {
+                return BadRequest("No se encontraron folios en la primera columna del archivo");
+            }
 
             var result = await _reconciliationService.ReconcileFromUploadedFileAsync(foliosFromExcel, lineId);
 
