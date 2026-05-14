d4898250814d3dcbd540d80d0ebbfdcd6b1164c2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-05-14 12:27:17 -0600

    feat(Repository): Repostory con metodos para obtener el inventario FTN y procesar el archivo FTN

---
+++ Infrastructure/Repositories/FtnInventoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class FtnInventoryRepository : IFtnInventoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FtnInventoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateFtnRecordAsync(FtnInventory ftnRecord)
+        {
+            await _context.FtnInventory.AddAsync(ftnRecord);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<FtnInventory>> GetActiveFtnRecordsByLineAsync(int lineId)
+        {
+            return await _context.FtnInventory
+                    .Where(f => f.LineId == lineId && f.Status == "EN_TRANSITO")
+                    .ToListAsync();
+        }
+
+        public async Task BulkUpdateFtnRecordsAsync(IEnumerable<FtnInventory> ftnRecords)
+        {
+            _context.FtnInventory.UpdateRange(ftnRecords);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
