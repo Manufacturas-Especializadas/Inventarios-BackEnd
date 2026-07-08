c34625277aa8b2bba17a3b6be30b0ee38875523f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 07:56:33 -0600

    feat(service): configuracion del nugget de MailKit

---
+++ Infrastructure/Services/EmailService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Options;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using Application.DTOs;
+using System.Diagnostics;
+using Application.Interfaces;
+
+namespace Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailService(IOptions<EmailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task SendGlobalNotificationAsync(string subject, string messageBody, List<string> recipients)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(_settings.SenderName, _settings.SenderEmail));
+
+            foreach (var address in recipients)
+            {
+                if (!string.IsNullOrEmpty(address))
+                {
+                    email.Bcc.Add(MailboxAddress.Parse(address));
+                }
+            }
+
+            email.Subject = subject;
+            var builder = new BodyBuilder { HtmlBody = messageBody };
+            email.Body = builder.ToMessageBody();
+
+            await SendViaSmtpAsync(email);
+        }
+
+        private async Task SendViaSmtpAsync(MimeMessage email)
+        {
+            using var smtp = new SmtpClient();
+            smtp.Timeout = 15000;
+
+            try
+            {
+                await smtp.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(_settings.Username, _settings.Password);
+
+                await smtp.SendAsync(email);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"[ERROR EMAIL]: {ex.Message}");
+                throw new Exception($"Error enviando correo a través de {_settings.Host}: {ex.Message}");
+            }
+            finally
+            {
+                await smtp.DisconnectAsync(true);
+            }
+        }
+    }
+}
