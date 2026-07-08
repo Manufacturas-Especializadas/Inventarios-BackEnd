1100cd48791a8f1926d96618c09063c799b8b9a2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 07:54:41 -0600

    feat(IService): interfaz para el envio de reporte

---
+++ Application/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendGlobalNotificationAsync(string subject, string messageBody, List<string> recipients);
+    }
+}
